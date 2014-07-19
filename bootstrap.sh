9c73b704a072e787226c28165fa5f7d67262092a
Ciaran Downey <me@ciarand.me>
2014-07-18 18:41:38 -0700

    Rename site.yml -> bootstrap.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,4 @@ ansible -i hosts all -S --ask-su-pass -u root -m raw -a \
     'env ASSUME_ALWAYS_YES=YES pkg bootstrap; \
     env ASSUME_ALWAYS_YES=YES pkg install python'
 
-ansible-playbook -i hosts site.yml -S --ask-su-pass -vvvv
+ansible-playbook -i hosts -S --ask-su-pass -vvvv bootstrap.yml
