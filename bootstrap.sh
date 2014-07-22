5495055a22b9ba2b0f1e5828fed07b6dd66c8c03
Ciaran Downey <me@ciarand.me>
2014-07-21 23:36:13 -0700

    Add an SSH echo warning message b/c I'm dumb

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,9 @@
 
 # -S means use su
 
-ansible -i hosts all -S --ask-su-pass -u root -m raw -a \
+echo "ASSUMING YOU'VE ALREADY COPIED THE SSH KEY OVER"
+
+ansible -i hosts all -S --ask-su-pass -vvvv -u root -m raw -a \
     'env ASSUME_ALWAYS_YES=YES pkg bootstrap; \
     env ASSUME_ALWAYS_YES=YES pkg install python'
 
